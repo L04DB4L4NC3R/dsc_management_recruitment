da812edcd0696f921299abe70c224270dc332d42
angadsharma1016 <angadsharma1016@gmail.com>
2018-12-11 02:11:11 +0530

    init

---
+++ frontend/index.js
@@ -0,0 +1,151 @@
+$(document).ready(function(){
+
+    var question_number = 1;
+
+    $('#final-submit').click(function(){
+        var regno = document.getElementById("regno").value;
+        var registrationcheck = {
+            registrationNumber : regno;
+        }
+        JSON.stringify(registrationcheck);
+        //this happens when the registration number is checked
+        $('.form').fadeOut(300);
+        setTimeout(function(){
+            $('.questions').fadeIn(600);
+        },300);
+    })
+
+    $('#question-next').click(function(){
+        question_number++;
+        $('.question').fadeOut(300);
+        setTimeout(function(){
+            if(question_number == 2){
+                $('#question-two').fadeIn(600);
+                $('#question-previous').fadeIn(600);
+            }
+            else if(question_number == 3){
+                $('#question-three').fadeIn(600);
+                $('#question-previous').fadeIn(600);
+            }
+            else if(question_number == 4){
+                $('#question-four').fadeIn(600);
+                $('#question-previous').fadeIn(600);
+            }
+            else if(question_number == 5){
+                $('#question-five').fadeIn(600);
+                $('#question-previous').fadeIn(600);
+            }
+            else if(question_number == 6){
+                $('#question-six').fadeIn(600);
+                $('#question-previous').fadeIn(600);
+            }
+            else if(question_number == 7){
+                $('#question-seven').fadeIn(600);
+                $('#question-previous').fadeIn(600);
+            }
+            else if(question_number == 8){
+                $('#question-eight').fadeIn(600);
+                $('#question-previous').fadeIn(600);
+            }
+            else if(question_number == 9){
+                $('#question-nine').fadeIn(600);
+                $('#question-previous').fadeIn(600);
+            }
+            else if(question_number == 10){
+                $('#question-ten').fadeIn(600);
+                $('#question-previous').fadeIn(600);
+                $('#question-next').text('Submit');
+            }
+            else if(question_number == 11){
+                $('.questions').fadeOut(300);
+                var answerone = document.getElementById("question-one").value;
+                var answertwo = document.getElementById("question-two").value;
+                var answerthree = document.getElementById("question-three").value;
+                var answerfour = document.getElementById("question-four").value;
+                var answerfive = document.getElementById("question-five").value;
+                var answersix = document.getElementById("question-six").value;
+                var answerseven = document.getElementById("question-seven").value;
+                var answereight = document.getElementById("question-eight").value;
+                var answernine = document.getElementById("question-nine").value;
+                var answerten = document.getElementById("question-ten").value;
+                setTimeout(function(){
+                    $('.thank-you').fadeIn(600);
+                    var answer = {
+                        answerOne : answerone,
+                        answerTwo : answertwo,
+                        answerThree : answerthree,
+                        answerFour : answerfour,
+                        answerFive : answerfive,
+                        answerSix : answersix,
+                        answerSeven : answerseven,
+                        answerNine : answernine,
+                        answerTen : answerten,
+                        answerTen : answerten
+                    }
+                    JSON.stringify(answer);
+                },300)
+            }
+        },300);
+    })
+
+    $('#question-previous').click(function(){
+        question_number--;
+        $('.question').fadeOut(300);
+        setTimeout(function(){
+            if(question_number == 1){
+                $('#question-one').fadeIn(600);
+                $('#question-next').fadeIn(600);
+                $('#question-previous').fadeOut(600);
+            }
+            else if(question_number == 2){
+                $('#question-two').fadeIn(600);
+                $('#question-next').fadeIn(600);
+                $('#question-previous').fadeIn(600);
+            }
+            else if(question_number == 3){
+                $('#question-three').fadeIn(600);
+                $('#question-next').fadeIn(600);
+                $('#question-previous').fadeIn(600);
+            }
+            else if(question_number == 4){
+                $('#question-four').fadeIn(600);
+                $('#question-previous').fadeIn(600);
+                $('#question-next').text('Next');
+            }
+            else if(question_number == 5){
+                $('#question-five').fadeIn(600);
+                $('#question-next').fadeIn(600);
+                $('#question-previous').fadeIn(600);
+            }
+
+            else if(question_number == 6){
+                $('#question-six').fadeIn(600);
+                $('#question-next').fadeIn(600);
+                $('#question-previous').fadeIn(600);
+            }
+            else if(question_number == 7){
+                $('#question-seven').fadeIn(600);
+                $('#question-next').fadeIn(600);
+                $('#question-previous').fadeIn(600);
+            }
+            else if(question_number == 8){
+                $('#question-eight').fadeIn(600);
+                $('#question-next').fadeIn(600);
+                $('#question-previous').fadeIn(600);
+            }
+            else if(question_number == 9){
+                $('#question-nine').fadeIn(600);
+                $('#question-next').fadeIn(600);
+                $('#question-previous').fadeIn(600);
+            }
+            else if(question_number == 10){
+                $('#question-ten').fadeIn(600);
+                $('#question-next').fadeIn(600);
+                $('#question-previous').fadeIn(600);
+                $('#question-next').text('Next');
+            }
+        },300);
+    })
+
+
+});
