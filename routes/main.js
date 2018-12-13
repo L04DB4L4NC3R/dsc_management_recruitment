89f6188b5e89dd561cdb31e6da9db9a2365bbd71
angadsharma1016 <angadsharma1016@gmail.com>
2018-12-13 22:38:53 +0530

    remove round 0 check

--- routes/main.js
+++ routes/main.js
@@ -6,20 +6,14 @@ const {
 
 
 router.post("/check",(req,res,next)=>{
-    users.findOne({registrationNumber:req.body.registrationNumber})
-    .then((u)=>{
-        if(!u)
-            return res.json({message:"Not filled round 0"})
-        if(!u.management)
-            return res.json({message:"You did not choose management as your domain in round 0"})
-        managers.findOne({registrationNumber:u.registrationNumber})
-        .then((ud)=>{
-            if(ud)
-                return res.json({message:"You have already attempted the test"})
-            req.session.user = req.body.registrationNumber;
-            return res.send("Done");
-        }).catch(next)
-    }).catch(next);
+    
+    managers.findOne({registrationNumber:req.body.registrationNumber})
+    .then((ud)=>{
+        if(ud)
+            return res.json({message:"You have already attempted the test"})
+        req.session.user = req.body.registrationNumber;
+        return res.send("Done");
+    }).catch(next)
 })
 
 
